6b4011b18351e38aaab120afc169b4bc89026cd8
신 수정 <jhw02045@naver.com>
2025-05-30 11:53:00 +0900

    올라가는 문 내려가는 문 [2025-05-30]

--- src/tutorial/loop.js
+++ src/tutorial/loop.js
@@ -1,7 +1,7 @@
 // 엘레베이터
 
-
-// 멈춘 층에서 문 열렸다 닫히게
+// 닫힘버튼 열림버튼
+// 높은 층을 먼저 누르고 낮은 층 눌렀을 때 낮은 층이 아직 안지났으면 해당층에서 먼저 멈추도록
 
 let currentFloorDisplay = document.getElementById("currentFloor");
 
@@ -34,6 +34,7 @@ for (i = 10; i >= 1; i --) {
             console.log(`${targetFloor}층 선택!`);
             moveToFloor();
         }
+
     });
 }
 
@@ -46,6 +47,9 @@ for (i = 10; i >= 1; i --) {
         isMoving = true;
 
         const interval = setInterval(() => {
+            const leftDoor = document.getElementById("opened-left-door");
+            const rightDoor = document.getElementById("opened-right-door");
+
             if(currentFloor < targetFloor) {
                 currentFloor ++;
                 currentFloorDisplay.textContent = `현재 층: ${currentFloor}`;
@@ -59,22 +63,43 @@ for (i = 10; i >= 1; i --) {
                 
                 console.log(`${targetFloor}층 도착!`);
                 
-                    currentFloorDisplay.textContent = `현재 층: ${currentFloor}`;
-                        
-                    const buttonsColor = document.querySelectorAll("#pushButton button");
-                    buttonsColor.forEach(btn => {
-                        if(Number(btn.textContent) === targetFloor) {
-                            btn.style.backgroundColor = "";
-                        }
-                    });
-                    
-                    setTimeout(() => {
-                        pressedFloor.shift();
-                        isMoving = false;
-                        moveToFloor();
-                    }, 5000);
-                };
+                leftDoor.classList.remove("close-left");
+                rightDoor.classList.remove("close-right");
 
-        }, 1000);
+                leftDoor.classList.add("open-left");
+                rightDoor.classList.add("open-right");
+                
+                console.log("문이 열립니다.");
+
+                setTimeout(() => {
+                leftDoor.classList.remove("open-left");
+                rightDoor.classList.remove("open-right");
+
+                leftDoor.classList.add("close-left");
+                rightDoor.classList.add("close-right");
+                
+                console.log("문이 닫힙니다.");
+                }, 5000);
+
+                currentFloorDisplay.textContent = `현재 층: ${currentFloor}`;
+
+                const buttonsColor = document.querySelectorAll("#pushButton button");
+                buttonsColor.forEach(btn => {
+                    if(Number(btn.textContent) === targetFloor) {
+                        btn.style.backgroundColor = "";
+                    }
+                });
+                
+                
+
+                
+                setTimeout(() => {
+                    pressedFloor.shift();
+                    isMoving = false;
+                    moveToFloor();
+                }, 5000);
+            };
+
+        }, 2000);
     }
        
