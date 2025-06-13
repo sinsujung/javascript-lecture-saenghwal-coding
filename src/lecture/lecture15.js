6bc5f9d086a31eefd8f232387befae53f4510565
신 수정 <jhw02045@naver.com>
2025-06-13 10:46:52 +0900

    생활코딩 모듈[2025-06-13]

--- src/lecture/lecture15.js
+++ src/lecture/lecture15.js
@@ -1,12 +1,17 @@
 // 객체지향 프로그래밍
 // this -> 약속된 변수(함수를 소유하고 있는 객체를 가르키고 있는 변수)
-var grades = {
-    'list': {'egoing': 10, 'k8805': 6, 'sorialgi': 80},
-    'show' : function() {
-        for(var name in this.list) {
-            console.log(name + ' : ' + this.list[name] + "<br/>");
-        }
-    }
-}
+// var grades = {
+//     'list': {'egoing': 10, 'k8805': 6, 'sorialgi': 80},
+//     'show' : function() {
+//         for(var name in this.list) {
+//             console.log(name + ' : ' + this.list[name] + "<br/>");
+//         }
+//     }
+// }
 
-grades.show();
+// grades.show();
+
+function welcome() {
+    return 'Hello world'
+}
+alert(welcome());
