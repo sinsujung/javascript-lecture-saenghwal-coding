bbfb0744e4af680685baf75787a4876c7ef35c98
신 수정 <jhw02045@naver.com>
2025-06-18 14:25:03 +0900

    생활코딩 스코프 [2025-06-18]

--- src/lecture/lecture17.js
+++ src/lecture/lecture17.js
@@ -32,3 +32,45 @@ for(var i = 0; i < 5; i++) {
     a();
     document.write(i);
 }
+
+/**
+ *  전역변수를 사용하는 법
+ * 
+ *  -> 불가피하게 전역변수를 사용해야 하는 경우는 하나의 객체를 전역변수로 만들고 객체의 속성으로 변수를 관리하는 방법을 사용한다.
+ */
+
+(function() {
+    var MYAPP = {}
+    MYAPP.calculator = {
+        'left' : null,
+        'right' : null
+    }
+    MYAPP.coordinate = {
+        'left' : null,
+        'right' : null
+    }
+    MYAPP.calculator.left = 10;
+    MYAPP.calculator.right = 20;
+    function sum() {
+        return MYAPP.calculator.left + MYAPP.calculator.right;
+    }
+    document.write(sum());
+}())
+
+/**
+ * 정적 유효범위
+ * 
+ * -> 자바스크립트는 함수가 선언된 시점에서의 유효범위를 갖는다. 이러한 유효범위의 방식을 정적 유효범위(static scoping), 혹은 렉시컬(lexical scoping)이라고 한다.
+ */
+var i = 5;
+
+function a () {
+    var i = 10;
+    b();
+}
+
+function b() {
+    document.write(i);
+}
+
+a(); 
